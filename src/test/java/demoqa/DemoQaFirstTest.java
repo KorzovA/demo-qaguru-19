204723aa903b71c40df66248118440efb47eb067
Anton Korzov <zthetoxa@gmail.com>
2023-03-30 17:18:49 +0300

    Comment

--- src/test/java/demoqa/DemoQaFirstTest.java
+++ src/test/java/demoqa/DemoQaFirstTest.java
@@ -17,7 +17,7 @@ public class DemoQaFirstTest {
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         open("https://demoqa.com/automation-practice-form");
-        //нет у меня ни каикх банеров рекламных и все проходит , ну пусть будет так
+        /*нет у меня ни каикх банеров рекламных и все проходит , ну пусть будет так*/
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Anton");
