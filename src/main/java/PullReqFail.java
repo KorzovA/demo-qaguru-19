872ea627a49f6e1538767021ddac1a108e715526
Anton Korzov <zthetoxa@gmail.com>
2023-05-23 21:30:12 +0300

    conflict

--- src/main/java/PullReqFail.java
+++ src/main/java/PullReqFail.java
@@ -1,6 +1,7 @@
 public class PullReqFail {
-    private static String msg = "He is woman!!!";
+
     public static void main(String[] args) {
-        System.out.println(msg);
+        String paint = "msg";
+        System.out.println(paint);
     }
 }
