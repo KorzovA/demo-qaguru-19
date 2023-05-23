85fc4d075091e0de7f165a8d97ecb1dcb898b703
Anton Korzov <zthetoxa@gmail.com>
2023-05-24 01:38:23 +0300

    4

---
+++ src/test/java/Korzov/SelenideHomeWork.java
@@ -0,0 +1,29 @@
+package Korzov;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideHomeWork extends TestBase {
+    @Test
+    void selenideLessonTwo() {
+        // Configuration.holdBrowserOpen = true;
+        open("https://github.com");
+        //  $(".Header-old").shouldHave(text("Solutions"));
+        $(".Header-old").$(byText("Solutions")).hover();
+        $(".border-bottom").$(byText("Enterprise")).click();
+        $(".application-main").shouldHave(text("Build like the best"));
+    }
+    @Test
+    void thirdLesson() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-more-pages-link").$("[type='button']").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
+
